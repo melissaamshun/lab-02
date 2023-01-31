20aa39a28e1cdeea3dea94adffb0fce6df56bdb5
melissaamshun <mshun@usc.edugit config --global user.email mshun@usc.edu>
2023-01-31 15:38:35 -0800

    finished client

--- tcp_client.cpp
+++ tcp_client.cpp
@@ -22,27 +22,34 @@ int main(int argc, char const *argv[])
 	// check to see if user input is valid
 	char socket_read_buffer[1024];
 	
+	
 	// TODO: Fill out the server ip and port
-	std::string server_ip = "10.26.76.194";
+	std::string server_ip = "10.26.117.14";
 	std::string server_port = "6666";
+	
 
 	int opt = 1;
 	int client_fd = -1;
 
 	// TODO: Create a TCP socket()
 	int sockfd, n;
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-
+	client_fd = socket(AF_INET, SOCK_STREAM, 0);
+	
+	
 	// Enable reusing address and port
-	if(setsockopt(client_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) { 
+	
+	if(setsockopt(client_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt) ) ) { 
 		return -1;
 	}
 	
+	//printf("test\n");
+	
 	// Check if the client socket was set up properly
-	if(client_fd == -1){
+	if(client_fd == -1){  
 		printf("Error- Socket setup failed");
 		return -1;
 	}
+	
 	
 	// Helping you out by pepping the struct for connecting to the server
 	struct addrinfo hints;
@@ -53,34 +60,32 @@ int main(int argc, char const *argv[])
 	getaddrinfo(server_ip.c_str(), server_port.c_str(), &hints, &server_addr);
 
 	// TODO: Connect() to the server (hint: you'll need to use server_addr)
-	if (connect(sockfd, server_addr->ai_addr, server_addr->ai_addrlen)<0){
+	if (connect(client_fd, server_addr->ai_addr, server_addr->ai_addrlen)<0){
         	error("ERROR connecting");
         }
 	
-
 	// TODO: Retreive user input
-	std::printf("Enter message:" );
+	printf("Enter message:" );
 	bzero(socket_read_buffer,strlen(socket_read_buffer));
-	fgets(socket_read_buffer, strlen(socket_read_buffer), stdin);
-	
+	scanf("%19[^\n]", socket_read_buffer);
 	
 	// TODO: Send() the user input to the server
-       	n = write(sockfd,socket_read_buffer,strlen(socket_read_buffer));
+       	n = write(client_fd,socket_read_buffer,256);
        	if (n<0){
        		error("ERROR writing");
        	}
-       	bzero(socket_read_buffer,strlen(socket_read_buffer));
+       	//bzero(socket_read_buffer,strlen(socket_read_buffer));
 	
 	// TODO: Recieve any messages from the server and print it here. Don't 
 	// forget to make sure the string is null terminated!
-	n = read(sockfd,socket_read_buffer,strlen(socket_read_buffer));
+	n = read(client_fd,socket_read_buffer,18);
 	if (n<0){
        		error("ERROR reading");
        	}
-	std::printf("%s\n",socket_read_buffer);
+	printf("%s\n",socket_read_buffer);
 	
 	// TODO: Close() the socket
-	close(sockfd);
+	close(client_fd);
 
 	return 0; 
 } 
